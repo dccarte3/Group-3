4f6800895e67fb6cc522b151a5848111c67fb71c
Dylan Carter <dccarte3@asu.edu>
2021-02-16 00:43:05 -0700

    added server to add games

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var path = require('path');
+var fs = require('fs');
+var app = express();
+const PORT = 3000
+
+var server = app.listen(PORT, () => {
+    console.log('App is listening on port: ' + PORT)
+});
+
+app.use(express.static('website'));
+
+app.get('/newgame', function (req, res) {
+    let data = req.query
+    let games = getList();
+    // let game = JSON.stringify(data)
+    games.push(data);
+    let newGames = JSON.stringify(games);
+    fs.writeFile('games.json', newGames, (err) => {
+        console.log("Game saved");
+    })
+    res.redirect('/');
+})
+
+function getList() {
+    let data = fs.readFileSync('games.json');
+    let games = JSON.parse(data);
+    return games;
+}
